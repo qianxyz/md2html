d0e9a068742e7cd9194c42ebe1ddecbc2ac78c59
Qianxy <xyqian67@gmail.com>
2023-08-11 13:25:08 -0700

    Refactor into functions

--- md2html.go
+++ md2html.go
@@ -5,9 +5,54 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"log"
 	"net/http"
 	"os"
 )
+
+// Convert the markdown string to a html string.
+// Both strings are represented as byte slices.
+func convert(md []byte, client *http.Client) ([]byte, error) {
+	payload := map[string]string{
+		"text": string(md),
+	}
+
+	body, err := json.Marshal(payload)
+	if err != nil {
+		return nil, err
+	}
+
+	url := "https://api.github.com/markdown"
+	req, err := http.NewRequest("POST", url, bytes.NewReader(body))
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("accept", "application/vnd.github+json")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	respBody, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return respBody, nil
+}
+
+// Serve the html string.
+func serve(html []byte) {
+	fmt.Println("Serving at http://0.0.0.0:8080 ...")
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/html")
+		w.Write(html)
+	})
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
 
 func main() {
 	if len(os.Args) != 2 {
@@ -17,47 +62,16 @@ func main() {
 
 	path := os.Args[1]
 
-	content, err := os.ReadFile(path)
+	md, err := os.ReadFile(path)
 	if err != nil {
-		fmt.Printf("Error: %v\n", err)
-		return
+		log.Fatal(err)
 	}
-
-	payload := map[string]string{
-		"text": string(content),
-	}
-
-	body, err := json.Marshal(payload)
-	if err != nil {
-		fmt.Printf("Error: %v", err)
-		return
-	}
-
-	url := "https://api.github.com/markdown"
-	req, err := http.NewRequest("POST", url, bytes.NewReader(body))
-	if err != nil {
-		fmt.Printf("Error: %v", err)
-		return
-	}
-	req.Header.Set("accept", "application/vnd.github+json")
 
 	client := &http.Client{}
-	resp, err := client.Do(req)
+	html, err := convert(md, client)
 	if err != nil {
-		fmt.Printf("Error: %v", err)
-		return
-	}
-	defer resp.Body.Close()
-
-	respBody, err := io.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Printf("Error: %v", err)
-		return
+		log.Fatal(err)
 	}
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "text/html")
-		w.Write(respBody)
-	})
-	http.ListenAndServe(":8080", nil)
+	serve(html)
 }
