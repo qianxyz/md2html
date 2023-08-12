e12bfc16ce6146c387faf9a21334f38f8d262b04
Qianxy <xyqian67@gmail.com>
2023-08-11 18:22:38 -0700

    Add live preview feature

--- md2html.go
+++ md2html.go
@@ -9,56 +9,24 @@ import (
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/fsnotify/fsnotify"
 )
 
-// Convert the markdown string to a html string.
-// Both strings are represented as byte slices.
-func convert(md []byte, client *http.Client) ([]byte, error) {
-	payload := map[string]string{
-		"text": string(md),
-	}
+var (
+	// The HTML string to be rendered.
+	// TODO: Add lock
+	rendered []byte
 
-	body, err := json.Marshal(payload)
-	if err != nil {
-		return nil, err
-	}
+	// The path of the Markdown file.
+	mdPath string
+	// The port to serve on.
+	port int
 
-	url := "https://api.github.com/markdown"
-	req, err := http.NewRequest("POST", url, bytes.NewReader(body))
-	if err != nil {
-		return nil, err
-	}
-	req.Header.Set("accept", "application/vnd.github+json")
+	client = &http.Client{}
+)
 
-	resp, err := client.Do(req)
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-
-	respBody, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return nil, err
-	}
-
-	return respBody, nil
-}
-
-// Serve the html string.
-func serve(html []byte, port int) {
-	fmt.Printf("Serving at http://0.0.0.0:%d ...\n", port)
-
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "text/html")
-		w.Write(html)
-	})
-
-	addr := fmt.Sprintf(":%d", port)
-	log.Fatal(http.ListenAndServe(addr, nil))
-}
-
-// Parse the command line arguments.
-func parseArgs() (string, int) {
+func init() {
 	pFlag := flag.Int("p", 8080, "port (default: 8080)")
 	flag.Parse()
 
@@ -67,23 +35,109 @@ func parseArgs() (string, int) {
 		os.Exit(0)
 	}
 
-	return flag.Arg(0), *pFlag
+	mdPath = flag.Arg(0)
+	port = *pFlag
+}
 
+// Read the file and convert its content into a HTML string,
+// which is stored in the global variable.
+func update() {
+	md, err := os.ReadFile(mdPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	payload := map[string]string{
+		"text": string(md),
+	}
+
+	body, err := json.Marshal(payload)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	url := "https://api.github.com/markdown"
+	req, err := http.NewRequest("POST", url, bytes.NewReader(body))
+	if err != nil {
+		log.Fatal(err)
+	}
+	req.Header.Set("accept", "application/vnd.github+json")
+
+	log.Println("Sending API request ...")
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	respBody, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	rendered = respBody
+	log.Printf("HTML updated at http://0.0.0.0:%d\n", port)
+}
+
+// Serve the html string.
+func serve() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/html")
+		w.Write(rendered)
+	})
+
+	addr := fmt.Sprintf(":%d", port)
+	log.Fatal(http.ListenAndServe(addr, nil))
+}
+
+// Watch a file, rerender HTML when it has been modified.
+func watch() {
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer watcher.Close()
+
+	addPath := func() {
+		err = watcher.Add(mdPath)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	addPath()
+
+	for {
+		select {
+		case event, ok := <-watcher.Events:
+			if !ok {
+				return
+			}
+			if event.Has(fsnotify.Write) {
+				log.Println(mdPath, "modified")
+				update()
+			}
+			// Some editor does not write to the file, but creates
+			// a new file and renames it (thus removing the
+			// original). In this case the watcher loses track of
+			// the file, so the path has to be added again.
+			if event.Has(fsnotify.Remove) {
+				log.Println(mdPath, "modified")
+				addPath()
+				update()
+			}
+		case err, ok := <-watcher.Errors:
+			if !ok {
+				return
+			}
+			log.Println("error:", err)
+		}
+	}
 }
 
 func main() {
-	path, port := parseArgs()
+	update()
 
-	md, err := os.ReadFile(path)
-	if err != nil {
-		log.Fatal(err)
-	}
+	go serve()
 
-	client := &http.Client{}
-	html, err := convert(md, client)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	serve(html, port)
+	watch()
 }
